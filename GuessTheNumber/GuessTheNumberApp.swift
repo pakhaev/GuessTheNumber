abc8ad719fd2ba14d09c23836a48d9bd424482da
Пахаев Хусейн <rvoltigo@gmail.com>
2023-06-19 09:34:17 +0300

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Khusain on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
